e35a7823559386078a985cecee5de4a3a1a6cd38
rohit-atree <rohit.george@atree.org>
2021-02-17 20:06:04 +0530

    changed route from pdf to documents

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PdfController;
+use App\Http\Controllers\DocumentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('pdf', PdfController::class);
+Route::resource('documents', DocumentController::class);
