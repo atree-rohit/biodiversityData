1f86d40e3d0293068864d26a01dc56966b8008d5
Rohit <rohit.george@atree.org>
2021-02-17 15:28:32 +0530

    npm run dev

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-require('./bootstrap');
+require('./bootstrap.js');
+
+import Vue from 'vue'
+
+//Main pages
+import App from './views/app'
+import AddModal from './views/add_pdf_modal.vue'
+
+
+const app = new Vue({
+    el: '#app',
+    components: {App, AddModal},
+    data: {
+    	showModal:true
+    },
+    methods: {
+    	add_pdf_button(){
+    		console.log("ADD Button Pressed");
+    	}
+    }
+});
+
