9587a6ba873a46b2086598b525de0d06be2bbb57
rohit-atree <rohit.george@atree.org>
2021-02-18 08:52:30 +0530

    added documents-table component

---
+++ resources/js/views/documents-table.vue
@@ -0,0 +1,29 @@
+<template>
+	<table class="table is-fullwidth">
+		<thead>
+			<tr>
+				<th class="is-info" v-for="f in fields">{{ f[1] }}</th>
+			</tr>
+		</thead>
+		<tbody class="">
+			<tr v-for="d in data">
+				<td>{{ d.id }}</td>
+				<td>{{ d.filename }}</td>
+				<td>{{ d.description }}</td>
+				<td v-if="!d.parsed"><a class="button is-small is-success" @click="parseDoc(d.id)">Parse document</a></td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+
+<script>
+export default {
+	props: ["fields", "data"],
+	methods: {
+		parseDoc(file){
+			window.location.href="documents/"+file;
+		}
+	}
+
+};
+</script>
