e330421eef940ebec436ce707903413e210e1024
Rohit <rohit.george@atree.org>
2021-02-23 15:25:21 +0530

    added store and index methods"

--- app/Http/Controllers/LimboDataController.php
+++ app/Http/Controllers/LimboDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Document;
 use App\Models\LimboData;
 use Illuminate\Http\Request;
 
@@ -35,10 +36,21 @@ class LimboDataController extends Controller
      */
     public function store(Request $request)
     {
-        // dd($request->all());
-        $x = $request->all();
-        // $y = json_encode($x);
-        return response()->json("Data added successfully!!");
+        $p = $request->page_no;
+        $pp = $request->per_page;
+        $doc = $request->document_id;
+        $range = json_encode([$p * $pp, ($p+1) * $pp]);
+
+        $limbo = new LimboData();
+        $limbo->range = $range;
+        $limbo->text = $request->data;
+        $limbo->changes = $request->changelog;
+        $limbo->document_id = $doc;
+        $limbo->save();
+
+        $response_text = "Data added for document with id: $doc for the range: $range successfully!!";
+
+        return response($response_text);
     }
 
     /**
@@ -47,6 +59,21 @@ class LimboDataController extends Controller
      * @param  \App\Models\LimboData  $limboData
      * @return \Illuminate\Http\Response
      */
+
+    public function parsed_data($id)
+    {
+        $data = Document::find($id)->with("limbo_data")->get()->first();
+        $fields = [
+            ['id', 'ID'],
+            ['range', 'Range'],
+            ['rows', 'Rows'],
+            ['parse', 'Parse']
+        ];
+        // dd($data->first());
+
+        return view("limbo_data.index", compact("data", "fields"));
+    }
+
     public function show(LimboData $limboData)
     {
         //
